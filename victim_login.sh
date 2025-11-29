a74c9fd1f4e8ccaf9953e633abc0a0613fa8605e
Shibe53 <raulserban05@gmail.com>
2025-11-29 01:16:49 +0100

    Fixed missing curl on victim
    Also added file for the arp attack

--- victim_login.sh
+++ victim_login.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Simple victim login script
+apk add curl
 while true; do
     echo "SENDING LOGIN REQUEST"
     curl -s -X POST -d "username=victim&password=secret" http://172.18.0.30/login
